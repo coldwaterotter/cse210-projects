5247afeac003577bb58a74e034f0dc9814551321
Collin Burt <bur21044@byui.edu>
2023-04-24 15:13:36 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine();
+        Console.WriteLine("Enter a list of numbers, enter 0 when finished.");
+        int entry = -1;
+        List<int> numbers;
+        numbers = new List<int>();
+
+        do 
+        {
+            Console.Write("Enter number: ");
+            entry = int.Parse(Console.ReadLine());
+
+            if (entry != 0)
+            {
+                numbers.Add(entry);
+            }
+
+        } while (entry != 0);
+
+        int sum = 0;
+        int largest = 0;
+        int total = 0;
+
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+            sum += num;
+            total += 1;
+        }
+        
+        float average = ((float)sum) / total;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        
+        Console.WriteLine();
     }
 }
