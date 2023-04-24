2501b582f2982d70a9f1ec7e5e3752d1978986d5
Collin Burt <bur21044@byui.edu>
2023-04-24 14:34:48 -0600

    Completed Prep 3 + Stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("\n Welcome to the number guessing game!");
+        string playAgain = "y";
+        do 
+        {
+        int answer = -1;
+        int guesses = 0;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                answer = int.Parse(Console.ReadLine());
+
+                if (answer == magicNumber)
+                {
+                    Console.WriteLine("Good Job! You guessed the magic number! \n");
+                }
+                else if (answer > magicNumber)
+                {
+                    Console.WriteLine("Guess again, the magic number is smaller.");
+                }
+                else if (answer < magicNumber)
+                {
+                    Console.WriteLine("Guess again, the magic number is bigger.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, I didn't recognize that, try agin");
+                }
+
+                guesses += 1;
+            
+            } while (answer != magicNumber);
+        Console.WriteLine($"It took you {guesses} guesses to solve the magic number!\n");
+        Console.Write("Would you like to play again? (y/n) ");
+        playAgain = Console.ReadLine();
+        } while (playAgain == "y");
+        Console.WriteLine();
     }
 }
