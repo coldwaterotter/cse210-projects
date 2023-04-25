79b0c9d9fa7cbd7632f79aac685d5f40ed5f962c
Collin Burt <bur21044@byui.edu>
2023-04-24 21:33:28 -0600

    Completed Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayGreeting();
+        string UserName = InputUserName();
+        int FavoriteNum = FavoriteNumber();
+        int square = SquaredNumber(FavoriteNum);
+        Result(square, UserName);
+        Console.WriteLine();
+    }
+    static void DisplayGreeting()
+    {
+        Console.WriteLine($"\n Welcome to the Program!");
+    }
+    static string InputUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int FavoriteNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favorite = int.Parse(Console.ReadLine());
+        return favorite;
+
+    }
+    static int SquaredNumber(int number)
+    {   
+        int square = number * number;
+        return square;
+    }
+
+    static void Result(int number, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
