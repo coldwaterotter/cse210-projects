5f4b4ff0b1d84f7cfef0b0e997a7ed359dcf0649
Collin Burt <bur21044@byui.edu>
2023-05-03 21:50:31 -0600

    Completed - Quotebook Code Along

---
+++ quotebook/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Board _board = new Board();
+
+/*
+Source source = new Source("None Were With Him","http://www.churchofjeuschrist.org");
+Quote holland = new Quote("Elder Holland","Because Jesus walked such a long, lonely path....",source);
+_board.AddQuote(holland);
+_board.FindQuotesByAuthor("holland");
+*/
+
+_board.AddQuote(
+    new Quote(
+        "Paul",
+        "God hath not given us the spirit of fear; but of power, and of love, and of a sound mind.",
+        new Source("2 Timothy 1:7")
+    )
+);
+
+_board.AddQuote(
+    new Quote(
+        "Sister Elaine S. Dalton",
+        "Work hard to achieve your dreams. Don't let discouragement or mistakes delay you.",
+        new Source(
+        "How to Dare Great Things",
+        "https://www.churchofjesuschrist.org/study/new-era/2012/03/how-to-dare-great-things?lang=eng"
+        )
+    )
+);
+
+_board.AddQuote(
+    new Quote(
+        "Alma",
+        "If ye have faith ye hope for things which are not seen, which are true.",
+        new Source(
+        "Alma 32:21"
+    ))
+);
+
+_board.AddQuote(
+    new Quote(
+        "Alma",
+        "Live in thanksgiving daily.",
+        new Source(
+        "Alma 34:38"
+    ))
+);
+
+_board.AddQuote(
+    new Quote(
+        "Alma",
+        "Counsel with the Lord in all thy doings, and he will direct thee for good.",
+        new Source(
+        "Alma 37:37"
+    ))
+);
+
+//_board.GetRandomQuote();
+
+Menu _menu = new Menu(_board);
+_menu.Display();
